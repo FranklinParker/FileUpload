90c1d243dccf7be993cd3c2bca674f4a0b4f5eb8
Franklin Parker <fparker@deliveryman.com>
2018-02-02 23:27:32 -0500

    read file sent

--- nodeserver/routes/file.js
+++ nodeserver/routes/file.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest: '../public/images'});
+const fs = require('fs');
+const upload = multer({dest: '../public/images'});
 
-router.post('/', upload.single('file'), function(req, res, next) {
+router.post('/', upload.single('file'), function (req, res, next) {
   console.log('req.file', req.file);
-  res.send({resp:'file'});
+  fs.readFile(req.file.path,'utf8', function (err, data) {
+    console.log('data', data);
+    res.send({resp: 'file'});
+  });
 });
 
 
